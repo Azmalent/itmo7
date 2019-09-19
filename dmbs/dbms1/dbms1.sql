0e89f557c2577acef5d4c64584ed86591028f5ae
Azmalent <dmitrysjestov@niuitmo.ru>
2019-09-19 14:43:45 +0300

    Update DBMS1

--- dmbs/dbms1/dbms1.sql
+++ dmbs/dbms1/dbms1.sql
@@ -1,26 +1,36 @@
 create or replace procedure print_mview_info
 as
-	my_schema varchar2(7);
+	schema varchar2(50);
 
-	cursor my_mviews is 
-		select name as view_name, referenced_name as table_name
+	cursor mviews is 
+		select name as view_name, referenced_name as table_name 
 		from sys.dba_dependencies 
-		where type = 'MATERIALIZED VIEW' and lower(owner) = lower(my_schema);
+		where type = 'MATERIALIZED VIEW' and lower(owner) = lower(schema);
 
 	n integer := 1;
+	total number := 0;
 begin
-	select sys_context('userenv', 'current_schema') into my_schema from dual;
-
 	dbms_output.enable();
-	dbms_output.put_line('Имя схемы: ' || my_schema || chr(13) || chr(10));
 
-	dbms_output.put_line(' No. Имя представления                    Имя таблицы');
-	dbms_output.put_line(' --- -----------------------------------  --------------------------------------');
+	select '&schema' into schema from dual;
+	
+	select count(1) into total 
+		from (select username from sys.dba_users) 
+		where lower(username) = lower(schema) and rownum = 1;
 
-	for row in my_mviews loop
-		dbms_output.put_line(lpad(n, 4) || ' ' || rpad(row.view_name, 37) || row.table_name);
-		n := n + 1;
-	end loop;
+	if total > 0 then
+		dbms_output.put_line('Имя схемы: ' || schema || chr(13) || chr(10));
+
+		dbms_output.put_line(' No. Имя представления                    Имя таблицы');
+		dbms_output.put_line(' --- -----------------------------------  --------------------------------------');
+
+		for row in mviews loop
+			dbms_output.put_line(lpad(n, 4) || ' ' || rpad(row.view_name, 37) || row.table_name);
+			n := n + 1;
+		end loop;
+	else
+		dbms_output.put_line('Схема ' || schema || ' не найдена.');
+	end if;
 
 end print_mview_info;
 /
@@ -30,3 +40,7 @@ prompt
 
 set serveroutput on format wrapped
 exec print_mview_info
+
+select name as view_name, referenced_name as table_name 
+		from sys.dba_dependencies 
+		where type = 'MATERIALIZED VIEW' 
